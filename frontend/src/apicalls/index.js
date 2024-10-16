00fccc6489b83aaf38349e777f8917b9af4d63c1
Anshu <pk993105@gmail.com>
2024-10-17 00:26:51 +0530

    backend url updated

--- frontend/src/apicalls/index.js
+++ frontend/src/apicalls/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:8000',
+    baseURL: 'https://quiz-app-backend-theta.vercel.app/',
     headers: {
         'authorization': `Bearer ${localStorage.getItem('token')}`
     }
